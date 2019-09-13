fa9d3d1df5ac9f03e0d71b29caaab28d4a189763
Borka Martin Orlov <borka.orlov@fieldforce.com>
2019-09-13 17:44:24 +0300

    Added context menu options to files too.

--- README.md
+++ README.md
@@ -2,19 +2,23 @@
 
 ## Features
 
-1) Explorer context menu for folders: Add folder to current workspace or open in new.
-2) Commands in command pallet (Extract to workspace/new workspace)
+1. Explorer context menu for folders: Add folder to current workspace or open in new.
+2. Commands in command pallet (Extract to workspace/new workspace)
 
 ## Release Notes
 
+### 1.0.5 - Friday 13th, September 2019
+
+* Can now extract parent folder from file context menu aswell.
+
 ### 1.0.4
 
-Tslint fixes.
+* Tslint fixes.
 
 ### 1.0.2
 
-Fixed issue with running on Mac/Linux operating system.
+* Fixed issue with running on Mac/Linux operating system.
 
 ### 1.0.0
 
-Initial release with basic functionality
+* Initial release with basic functionality
--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,6 @@
 import * as vscode from 'vscode';
-import { Uri } from 'vscode';
 
+import { Uri } from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
 
@@ -8,15 +8,15 @@ export function activate(context: vscode.ExtensionContext) {
 		'extension.extractToWorkspace',
 		(file = undefined) => {
 			var folderURI: Uri | null;
+
 			if (file !== undefined) {
-				folderURI = Uri.file(file.fsPath);
+				folderURI = getParentFolderURIFromPath(file);
 			} else {
 				folderURI = getActiveFolderUriFromEditor();
 			}
 
 			if (folderURI) {
-
-				let folders = folderURI.fsPath.replace('\\', '/').split('/');
+				let folders = folderURI.path.replace('\\', '/').split('/');
 				let folderName = folders[folders.length - 1];
 				vscode.workspace.updateWorkspaceFolders(0, null, {name: folderName, uri: folderURI});
 			}
@@ -25,11 +25,15 @@ export function activate(context: vscode.ExtensionContext) {
 
 
 	let extractToNewWorkspace = vscode.commands.registerCommand(
-		'extension.extractToNewWorkspace', (file = undefined) => {
+		'extension.extractToNewWorkspace',
+		(file = undefined) => {
+			let folderURI: Uri | null;
+
 			if (file !== undefined) {
-				vscode.commands.executeCommand('vscode.openFolder', file, true);
+				folderURI = getParentFolderURIFromPath(file);
+				vscode.commands.executeCommand('vscode.openFolder', folderURI, true);
 			} else {
-				let folderURI = getActiveFolderUriFromEditor();
+				folderURI = getActiveFolderUriFromEditor();
 				vscode.commands.executeCommand('vscode.openFolder', folderURI, true);
 			}
 		}
@@ -37,9 +41,15 @@ export function activate(context: vscode.ExtensionContext) {
 
 	context.subscriptions.push(extractToWorkspace);
 	context.subscriptions.push(extractToNewWorkspace);
-
 }
 
+
+/**
+ * Get the parent folder uri of the active editor file
+ *
+ * @returns {(Uri | null)}
+ * null if no active editor
+ */
 function getActiveFolderUriFromEditor(): Uri | null {
 	let activeEditor =  vscode.window.activeTextEditor;
 
@@ -51,6 +61,52 @@ function getActiveFolderUriFromEditor(): Uri | null {
 	} else {
 		return null;
 	}
+}
+
+
+/**
+ * Get the Folder URI if it's passed from the context menu of explorer
+ *
+ * @param {Uri} file
+ * @returns {Uri}
+ */
+function getParentFolderURIFromPath(file: Uri): Uri {
+	if (pathIsFolder(file)) {
+		return file;
+	}
+
+	let unix_path: string = file.fsPath.replace('\\', '/');
+	let parent_path: string = unix_path.substring(0, unix_path.lastIndexOf('/'));
+	let parent_uri: Uri = Uri.file(parent_path);
+
+	return parent_uri;
+}
+
+
+/**
+ * Checks if the path is a folder or a file
+ *
+ * @param {Uri} path
+ * @returns {Boolean}
+ */
+function pathIsFolder(path: Uri): Boolean {
+	// Convert \ to /
+	let unix_path = path.fsPath.replace('\\', '/');
+
+	// Get path segments
+	let segments: String[] = unix_path.split('/');
+
+	// Get last segment
+	let last_segment: String = segments[segments.length - 1];
+
+	// Check for . in segment
+	let has_dot: Number = last_segment.indexOf('.');
+
+	if (has_dot >= 0) {
+		return false;
+	}
+
+	return true;
 }
 
 
